0fa9b9be434c9fddc04fb675dfc4d93be9f1af92
taek105 <ht5172@gmail.com>
2024-05-29 19:06:12 +0900

    hi home

--- src/main/java/com/example/pushserver/PushServerApplication.java
+++ src/main/java/com/example/pushserver/PushServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PushServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PushServerApplication.class, args);
+        System.out.println("hi home");
     }
 
 }
