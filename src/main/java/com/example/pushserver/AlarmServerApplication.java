855120653c4d0522a72e158df26d7c47c0b4eddb
taek105 <ht5172@gmail.com>
2024-05-29 19:10:19 +0900

    fix name

--- src/main/java/com/example/pushserver/AlarmServerApplication.java
+++ src/main/java/com/example/pushserver/AlarmServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PushServerApplication {
+public class AlarmServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PushServerApplication.class, args);
+        SpringApplication.run(AlarmServerApplication.class, args);
         System.out.println("hi home");
     }
 
