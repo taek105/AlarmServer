9632eea2af5c3b18f2b0ead85007f1599634528d
taek105 <ht5172@gmail.com>
2024-05-29 19:16:15 +0900

    bb

--- src/main/java/com/example/alarmserver/AlarmServerApplication.java
+++ src/main/java/com/example/alarmserver/AlarmServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.pushserver;
+package com.example.alarmserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
